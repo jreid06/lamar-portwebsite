41387a97ddf53140a9df8a9d84425ef8aad8f023
Jason Reid <jasonbrandon9406@gmail.com>
2017-05-12 10:35:46 +0100

    Lamar project files

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+  // var svgAttributes = anime({
+  //   targets: '#svgAttributes polygon',
+  //   points: '64 128 8.574 96 8.574 32 64 0 119.426 32 119.426 96',
+  //   easing: 'easeInOutExpo'
+  // });
+
+  // var cssSelector = anime({
+  //   targets: '#cssSelector .el',
+  //   translateX: 250
+  // });
+
+
+
+
+})
